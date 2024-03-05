9efae162bc62bd996fa3068cce0f23219078b2d3
Suyeon Kim <alex@finddy.io>
2024-03-05 14:41:57 +0900

    Inter 폰트 적용

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
-	import { NavigationBar } from '$lib/components/homepage';
+	import { Footer } from '$lib/components/homepage';
 	import { i18n } from '$lib/i18n';
+	import '@fontsource-variable/inter';
 	import { ParaglideJS } from '@inlang/paraglide-js-adapter-sveltekit';
 	import '../app.css';
 </script>
 
 <ParaglideJS {i18n}>
-	<NavigationBar />
 	<main>
 		<slot />
 	</main>
+	<Footer />
 </ParaglideJS>
+
+<style>
+	:global(body) {
+		font-family: 'Inter Variable', sans-serif;
+	}
+</style>
